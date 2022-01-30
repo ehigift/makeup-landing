434d4c54efed7af5fbe4f694539b65417c7b115c
MfenaKaase <mfenakaase16@gmail.com>
2022-01-30 19:47:01 +0100

    created side nav and fixed main nav

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ window.addEventListener("DOMContentLoaded", () => {
 
     toggleBtn.addEventListener("click", () => {
         const sideMenu = document.querySelector(".side-menu");
-
+        const sideNav = document.querySelector(".side-nav");
         console.log("hey")
 
         sideMenu.classList.toggle("hide");
+        sideNav.classList.toggle("hide-sm");
+        if (toggleBtn.textContent == "close") {
+            toggleBtn.textContent = "menu";
+        } else {
+            toggleBtn.textContent = "close";
+        }
     })
 
 
